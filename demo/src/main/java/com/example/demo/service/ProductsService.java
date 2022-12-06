51810f5e3cd5242bd8fd87eec97ad3ed37560d56
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-06 13:39:13 +0100

    Removed Unused Imports

--- demo/src/main/java/com/example/demo/service/ProductsService.java
+++ demo/src/main/java/com/example/demo/service/ProductsService.java
@@ -5,10 +5,7 @@ import com.example.demo.repository.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
 import java.util.List;
-import java.util.UUID;
 
 
 @Service
