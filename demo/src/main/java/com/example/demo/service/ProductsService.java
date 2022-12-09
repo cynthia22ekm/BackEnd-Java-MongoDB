b119edec657d80a2dc95be9b45b1bb9cb7a3016b
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-09 13:32:07 +0100

    Chnaged the name of postproductresponse format

--- demo/src/main/java/com/example/demo/service/ProductsService.java
+++ demo/src/main/java/com/example/demo/service/ProductsService.java
@@ -23,34 +23,34 @@ public class ProductsService {
         return (List<Products>) repository.findByCategory(category);
     }
 
-    public PostProductsResponse sendProducts(Products product)
+    public PostProductResponse sendProducts(Products product)
     {
         Optional<Products> productById = repository.findById(product.getId());
         if(product.getCategory().length()==0) {
-            return new PostProductsResponse("Category is mandatory", product, HttpStatus.CONFLICT);
+            return new PostProductResponse("Category is mandatory", product, HttpStatus.CONFLICT);
         }
         if(productById.isPresent())
         {
-            return new PostProductsResponse("Product with same ID exist in Database", product, HttpStatus.CONFLICT);
+            return new PostProductResponse("Product with same ID exist in Database", product, HttpStatus.CONFLICT);
         }
         else {
             repository.save(product);
-            return new PostProductsResponse("Products Created Successfully", product, HttpStatus.OK);
+            return new PostProductResponse("Products Created Successfully", product, HttpStatus.OK);
         }
     }
 
 
-    public PostProductsResponse updateProducts(Products product) {
+    public PostProductResponse updateProducts(Products product) {
 
         Optional<Products> productById = repository.findById(product.getId());
         System.out.println("Product ID"+productById);
         if(!(productById.isPresent()))
         {
-            return new PostProductsResponse("Product does not Exist in Database", product, HttpStatus.CONFLICT);
+            return new PostProductResponse("Product does not Exist in Database", product, HttpStatus.CONFLICT);
         }
         else {
             repository.save(product);
-            return new PostProductsResponse("Products Updated Successfully", product, HttpStatus.OK);
+            return new PostProductResponse("Products Updated Successfully", product, HttpStatus.OK);
         }
     }
     public String deleteProducts(String productId )
