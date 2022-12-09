f7c45a192223b86fe085db133a03c42d0bbf990a
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-09 13:29:47 +0100

    Chnaged the name of the file

---
+++ demo/src/main/java/com/example/demo/service/GetResponse.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class GetResponse {
+    private List<User> data;
+    private HttpStatus status;
+    private String message;
+}
