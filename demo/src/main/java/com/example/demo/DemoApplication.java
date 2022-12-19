a7c688c1981c480991c769a82b1a323ba5c217c0
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-19 12:08:10 +0100

    Added web links for reference

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+//https://www.youtube.com/watch?v=x_-Tx6OFYLg&list=RDCMUCpUYkSDJpHug-vfC5S-NXRA&start_radio=1&rv=x_-Tx6OFYLg&t=560
+//https://www.youtube.com/watch?v=F0oK42CwCuo&list=RDCMUCpUYkSDJpHug-vfC5S-NXRA&index=17
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
