0bf80eacf82ec5e16bc40400250e9701528bbdf8
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-23 15:43:58 +0100

    Implemented CustomUserDetailService

---
+++ demo/src/main/java/com/example/demo/config/CustomUserDetailService.java
@@ -0,0 +1,27 @@
+package com.example.demo.config;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRegistrationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRegistrationRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+          User singleuser = repository.findByUserName(username);
+        if (singleuser == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        UserDetails user =  org.springframework.security.core.userdetails.User.withUsername(singleuser.getUserName()).password(singleuser.getPassword()).roles("USER").build();
+        System.out.println("user isnide customuserdetails"+user);
+        return user;
+    }
+}
