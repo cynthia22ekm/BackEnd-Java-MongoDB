3b61726e198949ed49a5ca539a30e58e141590f3
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-29 11:18:09 +0100

    Removed functions related to userID as its removed from user model

--- demo/src/main/java/com/example/demo/repository/UserRegistrationRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRegistrationRepository.java
@@ -1,16 +1,9 @@
 package com.example.demo.repository;
-
 import com.example.demo.model.User;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
 
-public interface UserRegistrationRepository extends MongoRepository<User, ObjectId> {
-
-    User findByUserId(String userId);
-
-    void deleteByUserId(String userId);
+public interface UserRegistrationRepository extends MongoRepository<User, Long> {
 
     User findByUserName(String userName);
 }
