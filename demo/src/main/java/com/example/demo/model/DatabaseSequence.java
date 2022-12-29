c5bd78b222ad1bb191e4ecf3cf89ef8c495c3e80
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-12-29 11:14:20 +0100

    Generated autoseq for user id

---
+++ demo/src/main/java/com/example/demo/model/DatabaseSequence.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "DatabaseSequences")
+public class DatabaseSequence {
+
+    @Id
+    private String id;
+    private long seq;
+}
