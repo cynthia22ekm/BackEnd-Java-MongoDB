15476b4c8b82e3c3e8fb250b705c1372fbd642e5
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-01-04 17:20:00 +0100

    Removed unused imports

--- demo/src/main/java/com/example/demo/repository/PasswordResetTokenRepository.java
+++ demo/src/main/java/com/example/demo/repository/PasswordResetTokenRepository.java
@@ -1,8 +1,6 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.PasswordResetToken;
-import com.example.demo.model.User;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface PasswordResetTokenRepository extends MongoRepository<PasswordResetToken, String> {
