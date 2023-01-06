be3ec014c915e83cd99555997ad05216a0005033
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-01-06 15:46:22 +0100

    Created a repository for saving token against email

--- demo/src/main/java/com/example/demo/repository/PasswordResetTokenRepository.java
+++ demo/src/main/java/com/example/demo/repository/PasswordResetTokenRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.PasswordResetToken;
+import com.example.demo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface PasswordResetTokenRepository extends MongoRepository<PasswordResetToken, String> {
 
+    PasswordResetToken findByEmail(String email);
+
+    PasswordResetToken findByToken(String token);
 }
