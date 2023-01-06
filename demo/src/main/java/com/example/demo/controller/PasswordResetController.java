37d81066bc4b31c3ca0eee67c5a71aa3574e1e0a
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-01-06 15:49:22 +0100

    Created controller for all token service

--- demo/src/main/java/com/example/demo/controller/PasswordResetController.java
+++ demo/src/main/java/com/example/demo/controller/PasswordResetController.java
@@ -1,21 +1,68 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.PasswordResetToken;
+import com.example.demo.config.EmailServiceImpl;
+import com.example.demo.model.ChangePassword;
+import com.example.demo.model.CreateToken;
+import com.example.demo.model.User;
+import com.example.demo.service.CreateTokenResponse;
 import com.example.demo.service.PasswordResetTokenService;
+import com.example.demo.service.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
-@RequestMapping("/token")
+@RequestMapping("/Forgot-Password")
 public class PasswordResetController {
 
     @Autowired
     private PasswordResetTokenService service;
 
-    @RequestMapping(value = "/CreateToken/{email}",method= RequestMethod.POST)
-    public String getToken(@PathVariable String email)
+    @Autowired
+    private EmailServiceImpl emailService;
+
+    @Autowired
+    private UserRegistrationService registrationService;
+
+    @RequestMapping(value = "/CreateToken",method= RequestMethod.POST)
+    public CreateTokenResponse getToken(@RequestBody CreateToken tokenData, HttpServletRequest request)
     {
-       return service.generateToken(email);
+
+        User user = registrationService.getUserByEmail(tokenData.getEmail());
+        if (user == null) {
+            return new CreateTokenResponse(null, "User does not exists in the database");
+        } else
+        {
+            String token =  service.generateToken(tokenData.getEmail()).getToken();
+            String path = "http://localhost:4200/reset-password";
+            emailService.constructResetTokenEmail(path, request.getLocale(),token, user);
+            return service.generateToken(tokenData.getEmail());
+        }
     }
+
+    @RequestMapping(value = "/ValidateToken/{token}",method= RequestMethod.GET)
+    public String redirectToChangePasswordPage(@PathVariable String token, Model model)
+    {
+        boolean isTokenValid = service.validatePasswordResetToken(token);
+        if(isTokenValid)
+        {
+
+            model.addAttribute("token", token);
+            System.out.println("Model is "+model);
+            return "Navigate to Reset Password page";
+        }
+        else {
+             return "Token is invalid or empty";
+        }
+    }
+
+    @RequestMapping(value = "/ResetPassword",method= RequestMethod.POST)
+    public String resetPassword(@RequestBody ChangePassword passwordData)
+    {
+    return service.updatePassword(passwordData);
+    }
+
 
 }
