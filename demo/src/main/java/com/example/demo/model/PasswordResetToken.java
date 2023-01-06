fcf8c529110dbe80aed8055ee68dd3ec9787b443
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-01-06 15:49:00 +0100

    Created model for generateToken

--- demo/src/main/java/com/example/demo/model/PasswordResetToken.java
+++ demo/src/main/java/com/example/demo/model/PasswordResetToken.java
@@ -1,59 +1,26 @@
 package com.example.demo.model;
 
-import org.bson.types.ObjectId;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection="PasswordResetToken")
 public class PasswordResetToken {
 
     @Id
     private String id;
     private String token;
-    private User user;
+    private String email;
     private Date expiryDate;
 
-    public PasswordResetToken(String id, String token, User user, Date expiryDate) {
-        this.id = id;
-        this.token = token;
-        this.user = user;
-        this.expiryDate = expiryDate;
-    }
 
-    public PasswordResetToken(String token, User user) {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public Date getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public void setExpiryDate(Date expiryDate) {
-        this.expiryDate = expiryDate;
-    }
 }
