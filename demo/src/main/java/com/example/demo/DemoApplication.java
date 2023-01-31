2f48b747d50711fda691ae5b4885e8935ec90d63
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-01-31 15:21:37 +0100

    Configures email service for generating an email

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,23 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.cors.CorsConfiguration;
 
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import java.util.Arrays;
+import java.util.Properties;
 
 @SpringBootApplication
 //https://www.youtube.com/watch?v=x_-Tx6OFYLg&list=RDCMUCpUYkSDJpHug-vfC5S-NXRA&start_radio=1&rv=x_-Tx6OFYLg&t=560
 //https://www.youtube.com/watch?v=F0oK42CwCuo&list=RDCMUCpUYkSDJpHug-vfC5S-NXRA&index=17
+//https://www.baeldung.com/spring-security-registration-i-forgot-my-password
+//https://www.baeldung.com/spring-email
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -35,6 +38,22 @@ public class DemoApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+		mailSender.setUsername("testemailcyn124@gmail.com");
+		mailSender.setPassword("ygokhgxjejlighry");
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
 	}
 
 	}
