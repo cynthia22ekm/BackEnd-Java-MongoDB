52b4c500e43bc29867644a2f6b92661db75cb1a6
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-01-31 15:28:40 +0100

    Modified configuration to include headers

--- demo/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,12 +1,19 @@
 package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 @Configuration
@@ -15,12 +22,24 @@ public class SwaggerConfig {
 
     @Bean
     public Docket productApi() {
+
+        RequestParameterBuilder tokenBuilder = new RequestParameterBuilder();
+        tokenBuilder
+                .name("Authorization")
+                .description("access_token")
+                .required(false)
+                .in("header")
+                .accepts(Collections.singleton(MediaType.APPLICATION_JSON))
+                .build();
+        List<RequestParameter> parameterList = new ArrayList<>();
+        tokenBuilder.name("Authorization").required(false).build();
+        parameterList.add(tokenBuilder.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(metaData());
+                .apiInfo(metaData()).globalRequestParameters(parameterList);
     }
 
 
