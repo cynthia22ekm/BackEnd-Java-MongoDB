140380128c798919c0bd08cae1057cff93492dad
cynthia22ekm <cynthiajoseph26291@gmail.com>
2023-01-31 15:26:58 +0100

    Created a controller for validate token generated during login

--- demo/src/main/java/com/example/demo/controller/UserRegistrationController.java
+++ demo/src/main/java/com/example/demo/controller/UserRegistrationController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
+import com.example.demo.config.JWTUtil;
 import com.example.demo.model.Login;
 import com.example.demo.model.User;
+import com.example.demo.model.ValidateTokenRequest;
+import com.example.demo.model.ValidateTokenResponse;
 import com.example.demo.service.GetUserResponse;
 import com.example.demo.service.LoginResponse;
 import com.example.demo.service.PostUserResponse;
@@ -20,6 +23,8 @@ public class UserRegistrationController {
 
     @Autowired
     private UserRegistrationService service;
+    @Autowired
+    private JWTUtil jwtUtil;
 
      @RequestMapping(value="/GetUsers", method= RequestMethod.GET)
     public GetUserResponse getUsers()
@@ -27,25 +32,48 @@ public class UserRegistrationController {
         return (GetUserResponse) service.getAllUsers();
     }
 
-    @RequestMapping(value="/GetUserByUserName/{userName}", method= RequestMethod.GET)
+    @RequestMapping(value="/GetUserByUserName/{username}", method= RequestMethod.GET)
     public User getUserWithUserName(@PathVariable String userName)
     {
         return service.getUserByUserName(userName);
     }
 
-    @RequestMapping(value="/GetUserByEmail/{email}", method= RequestMethod.GET)
+    /*@RequestMapping(value="/GetUserByEmail/{email}", method= RequestMethod.GET)
     public User getUserWithEmail(@PathVariable String email)
     {
         return service.getUserByEmail(email);
-    }
+    }*/
 
-    @RequestMapping(value="/Login", method=RequestMethod.POST)
+    @RequestMapping(value="/auth/Login", method=RequestMethod.POST)
     public LoginResponse UserLogin(@RequestBody Login data )
     {
        String username = data.getUsername();
        String password =data.getPassword();
+       /*Authentication authenticate = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(
+                                username, password
+                        )
+                );
+
+        UserDetails user = (UserDetails) authenticate.getPrincipal();*/
+
        return service.login(username, password);
     }
+
+
+    @RequestMapping(value="/validateToken", method=RequestMethod.POST)
+    public ValidateTokenResponse ValidateToken(@RequestBody ValidateTokenRequest data ) {
+        try {
+            String username = jwtUtil.getUsernameFromToken(data.getToken());
+            return service.ValidateLoginToken(username, data.getToken());
+        }
+        catch(Exception e){
+            return new ValidateTokenResponse(false, e.getMessage());
+        }
+
+    }
+
 
     @RequestMapping(value="/CreateUser", method=RequestMethod.POST)
     public PostUserResponse createUser(@RequestBody User user)
